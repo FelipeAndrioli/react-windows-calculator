b5adc4d5be0fdfb1c0dc592fbeb1759998d9796e
Felipe Andrioli <felipean@amdocs.com>
2020-12-08 11:03:05 -0300

    Removed file management and add code and database configuration to create the tables

--- backend/apirest/src/main/java/com/apirest/apirest/Expression.java
+++ backend/apirest/src/main/java/com/apirest/apirest/Expression.java
@@ -1,15 +1,23 @@
-package com.apirest.models;
+package com.apirest.apirest;
+
+import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="TB_EXPRESSION")
-public class Expression {
+public class Expression implements Serializable {
     
+    private static final long serialVersionUID = 1L;
+
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private long id;
+
     private double leftNumber;
     private double rightNumber;
     private char operation;
--- backend/apirest/src/main/java/com/apirest/apirest/User.java
+++ backend/apirest/src/main/java/com/apirest/apirest/User.java
@@ -1,4 +1,4 @@
-package com.apirest.models;
+package com.apirest.apirest;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
